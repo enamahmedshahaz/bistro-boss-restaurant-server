80a2dbff97c85361278ec91fa9842635cd43a66e
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-19 22:02:19 +0600

    Add API to get all menu and reviews

--- index.js
+++ index.js
@@ -26,22 +26,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    // Get the database and collection on which to run the operation
+    const database = client.db("BistroBossDB");
+    const menuCollection = database.collection("menu");
+    const reviewCollection = database.collection("reviews");
+
+    //API to get all menu items
+    app.get('/menu', async (req, res) => {
+      const cursor = menuCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    //API to get all reviews
+    app.get('/reviews', async (req, res) => {
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-  //  await client.close();
+    //  await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send('Bistro-Boss server is running...');
+  res.send('Bistro-Boss server is running...');
 });
 
 app.listen(port, () => {
-    console.log(`Bistro-Boss server is running on port ${port}...`);
+  console.log(`Bistro-Boss server is running on port ${port}...`);
 });
