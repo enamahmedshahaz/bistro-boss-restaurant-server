78ba18db62f3ad2d489d770bb290cfafb698230f
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-23 22:59:04 +0600

    Add API to delete cart items based on id

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmgmcoi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -61,6 +61,14 @@ async function run() {
       const query = { email: email };
       const cursor = cartCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //API to delete cart items based on id 
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
